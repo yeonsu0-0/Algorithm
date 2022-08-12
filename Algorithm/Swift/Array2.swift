4f1553d49bccb8672fcbb90c2a25ef754e1112e5
yeonsu <drawingbonny@gmail.com>
2022-08-12 22:09:57 +0900

    [programmers] 문자열 다루기 기본

---
+++ Algorithm/Swift/Array2.swift
@@ -0,0 +1,38 @@
+//
+//  Array2.swift
+//  Algorithm
+//
+//  Created by yeonsu on 8/12/22.
+//
+
+import Foundation
+
+
+/*
+ 
+문제 설명
+문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
+제한 사항
+s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+s는 영문 알파벳 대소문자 또는 0부터 9까지 숫자로 이루어져 있습니다.
+
+ */
+
+//첫 번째 풀이
+ 
+func solution(_ s:String) -> Bool {
+    if s.count != 4 || s.count != 6 {
+        if Int(s) == nil {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    else {
+        return true
+    }
+}
+ 
+
